c98849756e006d7b2a824a2050d9d9b9154fe661
peymanzarchini <peymanzarchini@outlook.com>
2024-09-30 22:09:06 +0330

    initial project

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
